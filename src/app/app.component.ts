6c5452db1307a75631c15b1ec114cd0e78563b69
Sepiqon <kalinuxhacker@gmail.com>
2021-02-07 16:27:12 +0100

    AddFreezer

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
-import { Component } from '@angular/core';
 
+import { Component, ViewChild } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +7,55 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'NebulousXp';
+  xp=0;
+  lvl=1;
+  lvls=0;
+  days=0;
+  target=0;
+  xpbday=0;
+  freezer=0;
+
+
+  ngOnInit(): void {
+	}
+
+	ngAfterViewInit(): void {
+		// afterViewInit code.
+		this.init();
+    }
+
+	init(): void {
+  }
+  xpCh(): number {
+    this.lvl = Math.floor(Math.sqrt(this.xp/this.freezer))+1;
+    var down = Math.pow(this.lvl-1,2)*this.freezer;
+    var up = Math.pow(this.lvl,2)*this.freezer;
+    this.lvls = Math.floor(((this.xp-down)/(up-down))*10000)/100;
+    this.days = Math.floor((Math.pow(this.target-1,2)*this.freezer-this.xp)/this.xpbday)+1
+    return this.xp;
+  }
+  lvlCh(): number {
+    var up=(Math.pow(this.lvl,2)*this.freezer);
+    var down = (Math.pow(this.lvl-1,2)*this.freezer);
+    this.xp =Math.floor(down + (up-down)*(this.lvls/100));
+    this.lvls =Math.floor(((this.xp-down)/(up-down))*10000)/100;
+    this.days = Math.floor((Math.pow(this.target-1,2)*this.freezer-this.xp)/this.xpbday)+1
+    return this.lvl;
+  }
+  lvlsCh(): number {
+    var up=(Math.pow(this.lvl,2)*this.freezer);
+    var down = (Math.pow(this.lvl-1,2)*this.freezer);
+    this.xp =Math.floor(down + (up-down)*(this.lvls/100));
+    this.lvls =Math.floor(((this.xp-down)/(up-down))*10000)/100;
+    this.days = Math.floor((Math.pow(this.target-1,2)*this.freezer-this.xp)/this.xpbday)+1
+    return this.lvls;
+  }
+  xpbdayCh(): number {
+    this.days = Math.floor((Math.pow(this.target-1,2)*this.freezer-this.xp)/this.xpbday)+1
+    return this.xpbday;
+  }
+  targetCh(): number {
+    this.days = Math.floor((Math.pow(this.target-1,2)*this.freezer-this.xp)/this.xpbday)+1
+    return this.target;
+  }
 }
